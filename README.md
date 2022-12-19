4f1334fd9ae1f09dc36f0c04a017a6a40fc1e6fa
Abubakar Abid <abubakar@huggingface.co>
2022-12-19 13:57:45 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+<p align="center">
+  <img width="200" src="gradio-discord.jpg">
+</p>
+
+# The Gradio Discord Bot<sup>BETA</sup>
+
+The Gradio Discord Bot is a way to use any [Hugging Face Space](https://hf.space) as a Bot from within your Discord server -- *all without writing any code*.
+
+Below, we show examples how to use the Bot in your server to:
+
+- 🌎 Translate between languages
+- 🗣️ Convert text to speech
+- 🔢 Do math calculations
+- 🖼️ Generate images
+
+All for free and without having to code anything!
+
+## Installation
+
+Installing the Gradio Discord Bot is very simple:
+
+1. Copy and paste the following link in your browser: https://discord.com/api/oauth2/authorize?client_id=1040198143695933501&permissions=294406716480&scope=bot  
+
+2. Choose which Discord server you'd like to add it to (you must have permissions to add Bots to that server of course)
+
+<p align="center">
+  <img width="600" alt="image" src="https://user-images.githubusercontent.com/1778297/208466659-00b23d23-fdc1-48e7-8868-dd248510acce.png">
+</p>
+
+3. Accept the permissions for the Gradio Bot
+
+<p align="center">
+ <img width="600" alt="image" src="https://user-images.githubusercontent.com/1778297/208466719-a6d64e0e-3aa1-4ead-90fc-23480f441b90.png">
+</p>
+
+That's it! You can confirm that the Gradio Discord Bot has been successfully installed in your server by going to the server and seeing if there is an account with the name `@Gradio Bot`:
+
+<p align="center">
+<img width="406" alt="image" src="https://user-images.githubusercontent.com/1778297/208508852-26e82415-d7df-429d-b2ef-0c4bd349687b.png">
+</p>
+
+
+## Usage
+
+
+
+## Contributing
+
+The Gradio Discord Bot is completely open-source. Feel free to open issues or pull requests in this repo to make it better!
---
+++ main.py
@@ -0,0 +1,84 @@
+import json
+import pathlib
+import re
+from typing import Dict
+
+import discord
+from discord.ext import commands
+import gradio as gr
+import requests
+
+
+with open("secrets.json") as fp:
+    discord_token = json.load(fp)["discord_token"]
+
+intents = discord.Intents(messages=True, guilds=True)
+
+bot = commands.Bot("", intents=intents)
+bot.guild_spaces: Dict[int, str] = {}
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+ 
+
+async def send_file_or_text(channel, file_or_text):
+    # if the file exists, send as a file
+    if pathlib.Path(str(file_or_text)).exists():
+        with open(file_or_text, "rb") as f:
+            return await channel.send(file=discord.File(f))
+    else:
+        return await channel.send(file_or_text)
+        
+ 
+@bot.event
+async def on_message(message: discord.Message):
+    if message.author == bot.user:
+        return
+    else:
+        if message.content:
+            content = message.content.replace("<@1040198143695933501>", "").strip()
+            guild = message.channel.guild
+            
+            print(content)
+            if content == "exit":
+                bot.guild_spaces.pop(guild.id, None)
+                await guild.me.edit(nick="Gradio Bot")
+            elif content.startswith("\"") or content.startswith("\'"):
+                if guild.id in bot.guild_spaces:
+                    params = re.split(r' (?=")', content)
+                    params = [p.strip("\'\"") for p in params]
+                    print(params)
+                    interface = bot.guild_spaces[guild.id]
+                    predictions = interface(*params)
+                    if isinstance(predictions, (tuple, list)):
+                        for p in predictions:
+                            await send_file_or_text(message.channel, p)
+                    else:
+                        await send_file_or_text(message.channel, predictions)
+                    return
+                else:
+                    await message.channel.send("No Space is currently running. Please type in the name of a Hugging Face Space name first, e.g. abidlabs/en2fr")
+                    await guild.me.edit(nick="Gradio Bot")                    
+            else:
+                iframe_url = (
+                    requests.get(f"https://huggingface.co/api/spaces/{content}/host")
+                    .json()
+                    .get("host")
+                )                
+                if iframe_url is None:
+                    return await message.channel.send(f"Space: {content} not found. If you'd like to make a prediction, enclose the inputs in quotation marks.")
+                else:
+                    await message.channel.send(f"Loading Space: https://huggingface.co/spaces/{content}...")
+                interface = gr.Interface.load(content, src="spaces")
+                bot.guild_spaces[guild.id] = interface
+                if len(content) > 32 - len("Gradio Bot []"):
+                    nickname = content[:32 - len("Gradio Bot []") - 3] + "..."
+                else:
+                    nickname = content
+                nickname = f"Gradio Bot [{nickname}]"
+                await guild.me.edit(nick=nickname)
+                await message.channel.send("Ready to make predictions! Type in your inputs and enclose them in quotation marks.")
+                    
+bot.run(discord_token)
